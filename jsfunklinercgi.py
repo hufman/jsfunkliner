699495dc2ecfc2464978903b2d18129e754821a3
Walter Huf <hufman@gmail.com>
2012-05-31 14:58:43 -0500

    Adds examples to the web interface

--- jsfunklinercgi.py
+++ jsfunklinercgi.py
@@ -47,7 +47,7 @@ def printContainer(attribute):
 	print('<textarea class="oldinput" name="old%s" rows="%s" cols="50" %s>%s</textarea>'%(attribute, rows, readonly, data))
 	print('<div class="container">')
 	print('<span class="label">%s</span>'%label)
-	print('<textarea name="%s" rows="%s" cols="50" %s>%s</textarea>'%(attribute, rows, readonly, data))
+	print('<textarea id="%s" name="%s" rows="%s" cols="50" %s>%s</textarea>'%(attribute, attribute, rows, readonly, data))
 	print('</div>')
 
 def printPage():
@@ -71,10 +71,61 @@ span.label {
 	opacity: 20%;
 }
 </style>
+<script type="text/javascript">
+var loader=function() {
+	var examples=[{
+			'desc':'Blank',
+			'library':'',
+			'snippet':''
+	},{
+			'desc':'Simple Test',
+			'library':'function foo(){\\n alert("bar");\\n}',
+			'snippet':'foo();'
+	},{
+			'desc':'Two Level Object',
+			'library':'object = function(){};\\nobject.prototype={\\n log : function(message) {\\n  this.reallog(message);\\n },\\n reallog: function (message) {\\n  if (typeof(console) != "undefined" && typeof(console.log) != "undefined")\\n   console.log(message);\\n  else\\n   window.alert(message)\\n }\\n};\\nobject2 = new object();',
+			'snippet':'object2.log("This is a test")'
+	}];
+
+	var box = document.getElementById('examples');
+	if (!box) return;
+	var library = document.getElementById('library');
+	var snippet = document.getElementById('snippet');
+
+	for (var i=0; i<examples.length; i++) {
+		var curexample = examples[i]
+		var mine = document.createElement('option');
+		mine.libraryContent = curexample['library'];
+		mine.snippetContent = curexample['snippet'];
+		mine.appendChild(document.createTextNode(curexample['desc']));
+		box.appendChild(mine);
+	}
+	var changed = function(e) {
+		var index = this.selectedIndex;
+		var cur = this.childNodes[index];
+		if (library.hasOwnProperty('textContent')) {
+			library.textContent = cur.libraryContent;
+			snippet.textContent = cur.snippetContent;
+		} else {
+			library.innerText = cur.libraryContent;
+			snippet.innerText = cur.snippetContent;
+		}
+	}
+		
+	if (box.attachEvent) box.attachEvent('onchange', changed);
+	else if (box.addEventListener) box.addEventListener('change', changed, false);
+}
+if (window.attachEvent) {window.attachEvent('onload', loader);}
+else if (window.addEventListener) {window.addEventListener('load', loader, false);}
+else {document.addEventListener('load', loader, false);}
+
+</script>
 </head>
 <body>
 """)
 	print('<p>Use this page to inline Javascript</p>')
+	print('<p>Any code in the Snippet box that calls functions in the Library box will have the function\'s body inlined</p>')
+	print('<p>Examples: <select id="examples"></select></p>')
 	print('<form action="" method="POST">')
 	printContainer('library')
 	printContainer('snippet')
